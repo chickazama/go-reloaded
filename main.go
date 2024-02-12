9b5ca250804e1558d2ce83a1a988ed84812fe6ea
Matthew Hope <matt@matthewhope.co.uk>
2024-02-12 12:13:06 +0000

    Define build function to build string output from final tokens

--- main.go
+++ main.go
@@ -166,3 +166,23 @@ func execute(tokens []string) []string {
 	// fmt.Println()
 	return ret
 }
+
+func build(tokens []string) string {
+	var sb strings.Builder
+	q := false
+	for i := range tokens {
+		sb.WriteString(tokens[i])
+		if tokens[i] == "'" {
+			q = !q
+			if q {
+				continue
+			}
+		}
+		if i < len(tokens)-1 && !re.MatchString(tokens[i+1]) {
+			sb.WriteRune(' ')
+		} else if i < len(tokens)-1 && tokens[i+1] == "'" && !q {
+			sb.WriteRune(' ')
+		}
+	}
+	return sb.String()
+}

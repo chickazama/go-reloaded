ad8164b3aa295a926509596ff4185f608472a0af
Matthew Hope <matt@matthewhope.co.uk>
2024-02-12 12:08:05 +0000

    Initialise command map with appropriate functions

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "regexp"
+import (
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 const (
 	alphaOffset = 'a' - 'A'
@@ -11,3 +16,40 @@ var (
 	re     = regexp.MustCompile("[.,!?:;']+")
 	cmdMap map[string]func(string) string
 )
+
+func init() {
+	cmdMap = make(map[string]func(string) string)
+	cmdMap["(bin)"] = bin
+	cmdMap["(hex)"] = hex
+	cmdMap["(up)"] = strings.ToUpper
+	cmdMap["(low)"] = strings.ToLower
+	cmdMap["(cap)"] = capitalize
+	cmdMap["(up"] = strings.ToUpper
+	cmdMap["(low"] = strings.ToLower
+	cmdMap["(cap"] = capitalize
+}
+
+func capitalize(str string) string {
+	out := []rune(str)
+	r := out[0]
+	if r >= 'a' && r <= 'z' {
+		out[0] -= alphaOffset
+	}
+	return string(out)
+}
+
+func hex(str string) string {
+	n, err := strconv.ParseInt(str, 16, 64)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return strconv.Itoa(int(n))
+}
+
+func bin(str string) string {
+	n, err := strconv.ParseInt(str, 2, 64)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return strconv.Itoa(int(n))
+}
